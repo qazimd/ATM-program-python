4e8f95c6ab51a65acc00b84eef29b225de48a7da
Qazim Dervishi <113587189+qazimd@users.noreply.github.com>
2022-11-07 10:40:59 +0100

    Update QD_ATM.py

--- QD_ATM.py
+++ QD_ATM.py
@@ -1,21 +1,26 @@
 class Account():
-    hasLogedIn = False
     def __init__(self, owner, balance):
         self.owner = owner
         self.balance = balance
 
     def deposit(self):
-        deposite_amount = int(input("\nYour currently have "+str(self.balance)+ " $\nHow much money do you want to deposite?\n"))
-        self.balance = self.balance + deposite_amount
-        print(str(deposite_amount)+"$ have been deposited to your account.\nThe current amount is "+str(self.balance)+"$")
+        try:
+            deposite_amount = int(input("\nYour currently have "+str(self.balance)+ " $\nHow much money do you want to deposite?\n"))
+            self.balance = self.balance + deposite_amount
+            print(str(deposite_amount)+"$ have been deposited to your account.\nThe current amount is "+str(self.balance)+"$")
+        except:
+            print('\nPlease enter a valid number!')
 
     def withdraw(self):
-        withdraw_amount = int(input("\nYour currently have "+str(self.balance)+ " $\nHow much money do you want to withdraw?\n"))
-        if withdraw_amount <= self.balance:
-            self.balance = self.balance - withdraw_amount
-            print(str(withdraw_amount)+ "$ have been withdrawn from your account.\nThe current amount in your Account is "+str(self.balance)+"$")
-        else:
-            print("You dont have enough money to perform this withdrawal")
+        try:
+            withdraw_amount = int(input("\nYour currently have "+str(self.balance)+ " $\nHow much money do you want to withdraw?\n"))
+            if withdraw_amount <= self.balance:
+                self.balance = self.balance - withdraw_amount
+                print(str(withdraw_amount)+ "$ have been withdrawn from your account.\nThe current amount in your Account is "+str(self.balance)+"$")
+            else:
+                print("You dont have enough money to perform this withdrawal")
+        except:
+            print('\nPlease enter a valid number!')
     
     def login(self):
         psw = input(f"Please put in the Account Password: ")
